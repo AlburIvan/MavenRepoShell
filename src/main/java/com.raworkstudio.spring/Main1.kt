a78e6ca0192cc72796f711aa51ac2f8b43fc2924
AlburIvan <albur.ivan@outlook.com>
2017-03-31 02:28:32 -0400

    Converted java file to kotlin

--- src/main/java/com.raworkstudio.spring/Main1.kt
+++ src/main/java/com.raworkstudio.spring/Main1.kt
@@ -1,25 +1,25 @@
-package com.raworkstudio.spring;
+package com.raworkstudio.spring
 
-import org.springframework.shell.Bootstrap;
+import org.springframework.shell.Bootstrap
 
-import java.io.IOException;
+import java.io.IOException
 
 /**
  * Driver class to run the helloworld example.
- *
+
  * @author Mark Pollack
- *
  */
-public class Main {
+object Main {
 
     /**
      * Main class that delegates to Spring Shell's Bootstrap class in order to simplify debugging inside an IDE
      * @param args
+     * *
      * @throws IOException
      */
-    public static void main(String[] args) throws IOException {
-        Bootstrap.main(args);
-
+    @Throws(IOException::class)
+    @JvmStatic fun main(args: Array<String>) {
+        Bootstrap.main(args)
     }
 
 }
