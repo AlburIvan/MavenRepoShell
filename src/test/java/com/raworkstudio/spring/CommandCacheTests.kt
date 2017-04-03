c470cb4cca273c815db21d22ccb6bddfd35cb46c
Ivan Alburquerque <Albur.Ivan@outlook.com>
2017-04-03 16:07:50 -0400

    Added more validation test

--- src/test/java/com/raworkstudio/spring/CommandCacheTests.kt
+++ src/test/java/com/raworkstudio/spring/CommandCacheTests.kt
@@ -1,5 +1,6 @@
 package com.raworkstudio.spring
 
+import com.raworkstudio.spring.configurations.CommandCacheProvider
 import org.hamcrest.MatcherAssert
 import org.junit.Before
 import org.junit.Test
@@ -17,10 +18,60 @@ class CommandCacheTests : AbstractShellIntegrationTest() {
 
     @Test
     fun `Check if file exists`(): Unit {
+        val commandCache = CommandCacheProvider()
+        MatcherAssert.assertThat("The cache file exists", commandCache.exists())
+    }
+
+    @Test
+    fun `Expects at least 1 entry after adding another element`(): Unit {
 
         val commandCache = CommandCacheProvider()
 
-        MatcherAssert.assertThat("The cache file exists", commandCache.exists() == true)
+        val oldEntryCount = commandCache.getFileAsObject().history.count()
+        println("old count: $oldEntryCount")
+
+        commandCache.put("butterknife")
+        commandCache.save()
+
+        val newEntryCount = commandCache.getFileAsObject().history.count()
+        println("new count: $newEntryCount")
+
+        MatcherAssert.assertThat("New entry was saved successfully", (newEntryCount > oldEntryCount) )
+    }
+
+
+    @Test
+    fun `Expects at least 1 less entry after removing an existing element`(): Unit {
+
+        val commandCache = CommandCacheProvider()
+
+        val oldEntryCount = commandCache.getFileAsObject().history.count()
+        println("old count: $oldEntryCount")
+
+        commandCache.delete("butterknife")
+        commandCache.save()
+
+        val newEntryCount = commandCache.getFileAsObject().history.count()
+        println("new count: $newEntryCount")
+
+        MatcherAssert.assertThat("New entry was deleted successfully", (newEntryCount < oldEntryCount) )
+    }
+
+    @Test
+    fun `Expects at the same number of entries after removing a NON existing element`(): Unit {
+
+        val commandCache = CommandCacheProvider()
+
+        val oldEntryCount = commandCache.getFileAsObject().history.count()
+        println("old count: $oldEntryCount")
+
+        commandCache.delete("./this.does not exit$")
+        commandCache.save()
+
+        val newEntryCount = commandCache.getFileAsObject().history.count()
+        println("new count: $newEntryCount")
+
+        MatcherAssert.assertThat("The entry was not deleted because it didn't exist", (newEntryCount == oldEntryCount) )
     }
 
 
--- src/test/java/com/raworkstudio/spring/SearchCommandTests.kt
+++ src/test/java/com/raworkstudio/spring/SearchCommandTests.kt
@@ -1,9 +1,11 @@
 package com.raworkstudio.spring
 
 import com.raworkstudio.spring.commands.SearchCommands
+import com.raworkstudio.spring.models.History
+import com.raworkstudio.spring.parsers.SeleniumParserImpl
 import org.hamcrest.MatcherAssert
 import org.junit.Test
-import org.springframework.shell.core.CommandResult
+import java.util.*
 
 /**
  * Created by Ivan Alburquerque on 3/30/2017
@@ -22,6 +24,24 @@ class SearchCommandTests : AbstractShellIntegrationTest() {
 
 
     @Test fun `Given an artifact name, When the artifact equals 'x', Then a list of artifact names should be returned`() {
+
+        //val commandResult = AbstractShellIntegrationTest.getShell().executeCommand("mvnsh --f --artifact butterknife")
+        var artifactMap: HashMap<String, History>
+
+
+        var parser = SeleniumParserImpl()
+
+        artifactMap = parser.queryArtifacts("butterknife")
+
+
+        artifactMap.forEach { s, history ->
+
+            println("$s | $history")
+        }
+
+
+
+        MatcherAssert.assertThat("...", true)
 
     }
 
